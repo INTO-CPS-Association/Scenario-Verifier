fb325817423a185e1e4cc60bd2d03f7e4a78f556
Simon Thrane Hansen <sth@eng.au.dk>
2020-12-14 14:43:37 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -8,3 +8,18 @@ mkdir -p release/
 rsync -a ./scripts ./release/
 rsync -a ./src/test/resources/ ./release/
 find ./target/scala-2.13/ -name '*.jar' -exec mv {} ./release/ \;
+cd release
+JARFILE=*.jar
+mkdir uppall_files_success
+for f in examples/*.conf
+do 
+    OUT=./uppall_files_success/$(basename ${f%.*}).xml
+    java -jar $JARFILE -m ${f} -o $OUT 
+done
+
+mkdir uppall_files_mistakes
+for f in common_mistakes/*.conf
+do 
+    OUT=./uppall_files_mistakes/$(basename ${f%.*}).xml
+    java -jar $JARFILE -m ${f} -o $OUT 
+done
