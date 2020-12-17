fc1b4782ae886eeeb93d1538b47270924b48badd
Simon Thrane Hansen <sth@eng.au.dk>
2020-12-17 08:30:04 +0100

    Added support for generating algebraic loop both GS and Jac

--- src/main/scala/synthesizer/Synthesizer.scala
+++ src/main/scala/synthesizer/Synthesizer.scala
@@ -1,11 +1,8 @@
 package synthesizer
 
-import core.Reactivity.{Value, reactive}
 import core._
-import sun.util.resources.Bundles.Strategy
 import synthesizer.LoopStrategy.{LoopStrategy, maximum}
 
-import scala.:+
 import scala.collection.convert.ImplicitConversions.`collection asJava`
 import scala.collection.mutable
 
@@ -19,61 +16,68 @@ class Synthesizer(scenarioModel: ScenarioModel, strategy: LoopStrategy) {
   var FMUsThatHaveStepped: mutable.HashSet[String] = new mutable.HashSet[String]()
 
   def formatInitLoop(scc: mutable.Buffer[Node]): InitializationInstruction = {
-    val outputPorts = {
-      graphBuilder.GetNodes.values.flatten.filter(o => scc.contains(o)).map { case GetNode(ref) => ref }.toList
-    }
+    val gets = graphBuilder.GetNodes.values.flatten.filter(o => scc.contains(o))
 
     var edgesInGraph = graphBuilder.initialEdges.filter(e => scc.contains(e.srcNode) && scc.contains(e.trgNode))
-    if(strategy == maximum)
-      //Remove all connections between FMUs
-      edgesInGraph = edgesInGraph.filterNot(e => outputPorts.contains(e.trgNode))
-    else{
+    if (strategy == maximum)
+    //Remove all connections between FMUs
+      edgesInGraph = edgesInGraph.filterNot(e => gets.contains(e.trgNode))
+    else {
       //Remove all connections to a single FMU
-      val fmu = outputPorts.groupBy(o => o.fmu)
-      val reducedList = outputPorts.filterNot(o => o.fmu == fmu.head._1)
+      val reducedList = gets.groupBy(o => o.port.fmu).head._2
       edgesInGraph = edgesInGraph.filter(e => reducedList.contains(e.trgNode))
     }
     val tarjanGraph: TarjanGraph[Node] = new TarjanGraph[Node](edgesInGraph)
 
-    AlgebraicLoopInit(outputPorts, tarjanGraph.topologicalSCC.flatten.map(formatInitialInstruction).toList)
+    AlgebraicLoopInit(gets.map(o => o.port).toList, tarjanGraph.topologicalSCC.flatten.map(formatInitialInstruction).toList)
   }
 
-/*
-  def formatAlgebraicLoop(scc: mutable.Buffer[Node]): CosimStepInstruction = {
+
+  def formatAlgebraicLoop(scc: mutable.Buffer[Node]): List[CosimStepInstruction] = {
     val steps = graphBuilder.stepNodes.filter(o => scc.contains(o))
     val gets = graphBuilder.GetNodes.values.flatten.filter(o => scc.contains(o))
     val setsDelayed = graphBuilder.SetNodesDelayed.values.flatten.filter(o => scc.contains(o))
-    val setsReactive = graphBuilder.SetNodesDelayed.values.flatten.filter(o => scc.contains(o))
+    val setsReactive = graphBuilder.SetNodesReactive.values.flatten.filter(o => scc.contains(o))
 
-    var edgesInGraph = graphBuilder.stepEdges.filter(e => scc.contains(e.srcNode) && scc.contains(e.trgNode))
-    val fmus = steps.map(o => o.name)
+    var edgesInSCC = graphBuilder.stepEdges.filter(e => scc.contains(e.srcNode) && scc.contains(e.trgNode))
+    val FMUs = steps.map(o => o.name)
+
+    val reactiveGets = gets.filter(o => (edgesInSCC.exists(edge => edge.srcNode == o && setsReactive.contains(edge.trgNode)))).toSet
 
     //Add restore and save nodes:
-    fmus.foreach(fmu => {
-      edgesInGraph+= (Edge[Node](SaveNode(fmu), RestoreNode(fmu)))
-      edgesInGraph+= (Edge[Node](SaveNode(fmu), DoStepNode(fmu)))
-      val setsFMU = setsDelayed.filter(o => o.port.fmu == fmu) ++ setsDelayed.filter(o => o.port.fmu == fmu)
-      setsFMU.foreach(s => {
-        edgesInGraph+= (Edge[Node](SaveNode(fmu), s))
+    FMUs.foreach(fmu => {
+      edgesInSCC += (Edge[Node](SaveNode(fmu), RestoreNode(fmu)))
+      edgesInSCC += (Edge[Node](SaveNode(fmu), DoStepNode(fmu)))
+      //val setsFMU = setsDelayed.filter(o => o.port.fmu == fmu) ++ setsReactive.filter(o => o.port.fmu == fmu)
+      (setsDelayed ++ setsReactive).foreach(s => {
+        edgesInSCC += (Edge[Node](SaveNode(fmu), s))
+      })
+      reactiveGets.foreach(g => {
+        edgesInSCC += (Edge[Node](g, RestoreNode(fmu)))
       })
     })
 
     //Remove all connections between FMUs
-    if(strategy == maximum)
+    if (strategy == maximum)
     //Remove all connections between FMUs
-      edgesInGraph = edgesInGraph.filterNot(e => setsReactive.toList.contains(e.trgNode))
-    else{
+      edgesInSCC = edgesInSCC.filterNot(e => setsReactive.toList.contains(e.trgNode) && gets.contains(e.srcNode))
+    else {
       //Remove all connections to a single FMU
-      val fmu = outputPorts.groupBy(o => o.fmu)
-      val reducedList = outputPorts.filterNot(o => o.fmu == fmu.head._1)
-      edgesInGraph = edgesInGraph.filter(e => reducedList.contains(e.trgNode))
+      val reducedList = setsReactive.groupBy(o => o.port.fmu).head._2
+      edgesInSCC = edgesInSCC.filterNot(e => reducedList.contains(e.trgNode) && gets.contains(e.srcNode))
     }
 
-    val tarjanGraph: TarjanGraph[Node] = new TarjanGraph[Node](edgesInGraph)
+    val tarjanGraph: TarjanGraph[Node] = new TarjanGraph[Node](edgesInSCC)
 
-    AlgebraicLoop(outputPorts.map { case GetNode(ref) => ref }, tarjanGraph.topologicalSCC.flatten.map().toList)
+    val instructions = tarjanGraph.topologicalSCC.flatten.map(formatStepInstruction(_, true)).filter(o => o match {
+      case RestoreState(fmu) => false
+      case SaveState(fmu) => false
+      case _ => true
+    }).toList
+    val saves = FMUs.map(o => formatStepInstruction(SaveNode(o), true)).toList
+    val restores = FMUs.map(o => formatStepInstruction(RestoreNode(o), true)).toList
+    saves.:+(AlgebraicLoop(reactiveGets.map(_.port).toList, instructions, restores))
   }
-*/
 
   def synthesizeInitialization(): List[InitializationInstruction] = {
     val tarjanGraph: TarjanGraph[Node] = new TarjanGraph[Node](graphBuilder.initialEdges)
@@ -92,15 +96,16 @@ class Synthesizer(scenarioModel: ScenarioModel, strategy: LoopStrategy) {
   def synthesizeStep(): List[CosimStepInstruction] = {
     val tarjanGraph: TarjanGraph[Node] = new TarjanGraph[Node](graphBuilder.stepEdges)
     val SCCs = tarjanGraph.topologicalSCC
-
-    SCCs.map(scc => {
+    var instructions : List[CosimStepInstruction] = List.empty[CosimStepInstruction]
+    SCCs.foreach(scc => {
       if (scc.size == 1) {
         //Currently only trivial SCC are considered
-        formatStepInstruction(scc.head, false)
+        instructions = instructions.:+(formatStepInstruction(scc.head, false))
       } else {
-        throw new UnsupportedOperationException()
+        instructions = instructions ++ formatAlgebraicLoop(scc)
       }
-    }).toList
+    })
+    instructions
   }
 
   def formatInitialInstruction(node: Node): InitializationInstruction = {
--- src/test/scala/SynthesizerTest.scala
+++ src/test/scala/SynthesizerTest.scala
@@ -2,25 +2,30 @@ import java.io.{File, PrintWriter}
 import java.nio.file.Files
 
 import cli.VerifyTA
-import core.{MasterModel, ModelEncoding, PortRef, ScenarioGenerator, ScenarioLoader}
+import core.{MasterModel, ModelEncoding, ScenarioGenerator, ScenarioLoader}
 import org.apache.commons.io.FileUtils
-import org.scalatest.Ignore
 import org.scalatest.flatspec._
 import org.scalatest.matchers._
+import synthesizer.LoopStrategy.{LoopStrategy, maximum, minimum}
 import synthesizer._
 
-class SynthesizerTest extends AnyFlatSpec with should.Matchers {
+class SynthesizerPositiveTest extends AnyFlatSpec with should.Matchers {
 
   def writeToTempFile(content: String) = {
+    /*
+    val file = new File("uppaal_.xml")
+    new PrintWriter(file) { write(content); close() }
+    file
+    */
     val file = Files.createTempFile("uppaal_", ".xml").toFile
     new PrintWriter(file) { write(content); close() }
     file
   }
 
-  def synthesizeAndVerify(resourcesFile: String) = {
+  def synthesizeAndVerify(resourcesFile: String, strategy: LoopStrategy = maximum) = {
     val conf = getClass.getResourceAsStream(resourcesFile)
     val scenario = ScenarioLoader.load(conf)
-    val synthesizer = new Synthesizer(scenario.scenario, LoopStrategy.maximum)
+    val synthesizer = new Synthesizer(scenario.scenario, strategy)
     val step = synthesizer.synthesizeStep()
     val init =  ScenarioLoader.generateEnterInitInstructions(scenario.scenario) ++ synthesizer.synthesizeInitialization() ++ ScenarioLoader.generateExitInitInstructions(scenario.scenario)
     val model = MasterModel(scenario.name, scenario.scenario, instantiation = scenario.instantiation, initialization = init.toList, cosimStep = step, terminate = scenario.terminate)
@@ -31,6 +36,7 @@ class SynthesizerTest extends AnyFlatSpec with should.Matchers {
     VerifyTA.verify(f) should be (0)
     FileUtils.deleteQuietly(f)
   }
+
 
   "Synthesizer" should "create valid Master Algorithm for Algebraic Initialization" in{
     synthesizeAndVerify("examples/algebraic_loop_initialization.conf")
@@ -43,8 +49,15 @@ class SynthesizerTest extends AnyFlatSpec with should.Matchers {
   "Synthesizer" should "create valid Master Algorithm for Industrial case study" in{
     synthesizeAndVerify("examples/industrial_casestudy.conf")
   }
-
-  ignore should "create valid Step procedure for Two Algebraic Loops" in {
+  "Synthesizer" should "create valid Step procedure for Two Algebraic Loops" in {
     synthesizeAndVerify("examples/two_algebraic_loops.conf")
+  }
+
+  "Synthesizer" should "create valid Jacboian Step procedure for Algebraic Loop" in {
+    synthesizeAndVerify("examples/algebraic_loop_msd_jac.conf")
+  }
+
+  "Synthesizer" should "create valid Gauss Seidel Step procedure for Algebraic Loop" in {
+    synthesizeAndVerify("examples/algebraic_loop_msd_gs.conf", minimum)
   }
 }
